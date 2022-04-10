d72f1610246dd3cd838480f105adfaff05c013c7
Anastasia Han <anastasia.han@yahoo.com>
2022-04-10 12:38:41 -0400

    Add Migrations Folder

---
+++ ParisBakery/Migrations/20220409193644_FlavorDescription.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParisBakery.Solution.Migrations
+{
+    public partial class FlavorDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Flavors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Flavors");
+        }
+    }
+}
